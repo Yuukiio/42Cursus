425026eda03b3f03c9e9ec498f0ca77363d29f9b
Adam Bolivar Doulim <abolivar@c3r7s4.42malaga.com>
2023-10-05 19:43:50 +0200

    a

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,10 +14,12 @@ void	*ft_memcpy(void *dst,  const void *src, size_t n)
 {
 	size_t i;
 
-	i = 0;
-	while (i < n) {
+	i = -1;
+    if (!dst && !src)
+        return (0);
+
+	while (++i < n) {
         *((char *)(dst + i)) = *((char *)(src + i));
-        i++;
     }
 	return (dst);
 }
