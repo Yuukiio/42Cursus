2fd7113775c98eba5a1cde83573dcd7a860ca745
Adam Bolivar Doulim <abolivar@c2r4s2.42malaga.com>
2023-10-07 15:56:26 +0200

    norminette changes

--- ft_bzero.c
+++ ft_bzero.c
@@ -10,22 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+
 void	ft_bzero(void *s, size_t n)
 {
-	size_t i;
-	unsigned char *str;
+	size_t			i;
+	unsigned char	*str;
 
 	i = 0;
 	str = (unsigned char *)s;
 	while (i < n)
 		*(str + i++) = 0;
 }
-/*
-#include <stdio.h>
-#include <string.h>
-int main(){
-	int s1[] = {1,2,3,4};
-	ft_bzero(s1, 2 * sizeof(int));
-	for (int i = 0; i < 4; i++)
-		printf("%d\n", s1[i]);
-}*/
