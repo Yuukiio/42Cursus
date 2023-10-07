21d1e42cfcf24345fd1552be54cecf82f4d196ab
Adam Bolivar Doulim <abolivar@c2r4s2.42malaga.com>
2023-10-07 15:51:53 +0200

    norminette changes

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,28 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
+
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char 	*s;
-	char 	*d;
+	char	*s;
+	char	*d;
 
 	s = (char *)src;
 	d = (char *)dst;
 	if (d > s)
 	{
 		while (len-- > 0)
-            *(d + len) = *(s + len);
+			*(d + len) = *(s + len);
 	}
 	else
 	{
-		ft_memcpy(dst, src,len);
+		ft_memcpy (dst, src, len);
 	}
 	return (dst);
 }
-/*int main ()
-{
-
-	printf("%s,\n",(char *)ft_memmove(strdup("asasasa"),"asdefsdc",5));
-	printf("%s",(char *)memmove(strdup("asasasa"),"asdefsdc",5));
-}*/
