265b59fd014212baa447cd06425dfdfd125e736b
Adam Bolivar Doulim <abolivar@c2r4s2.42malaga.com>
2023-10-07 16:25:29 +0200

    norminette changes

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -10,20 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-    size_t len_d;
-    size_t len_s;
-    size_t count;
 
-    len_d = ft_strlen(dst);
-    len_s = ft_strlen(src);
-    if (dstsize <= len_d)
-        return (len_s + dstsize);
-    count = len_d;
-    while (count < (dstsize - 1) && *src)
-        *(dst + count++) = *src++;
-    if (dstsize || len_d <= dstsize)
-        *(dst + count) = '\0';
-    return (len_d + len_s);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	len_d;
+	size_t	len_s;
+	size_t	count;
+
+	len_d = ft_strlen(dst);
+	len_s = ft_strlen(src);
+	if (dstsize <= len_d)
+		return (len_s + dstsize);
+	count = len_d;
+	while (count < (dstsize - 1) && *src)
+		*(dst + count++) = *src++;
+	if (dstsize || len_d <= dstsize)
+		*(dst + count) = '\0';
+	return (len_d + len_s);
 }
