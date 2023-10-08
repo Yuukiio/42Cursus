a178f52f5521553484590ffbb015b2c68c5a8541
Adam Bolivar Doulim <abolivar@c3r6s6.42malaga.com>
2023-10-08 06:19:04 +0200

    new files

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -9,3 +9,25 @@
 /*   Updated: 2023/09/23 01:24:11 by abolivar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+int ft_strncmp(const char *string1, const char *string2, size_t max_chars)
+{
+	size_t index;
+
+	index = 0;
+	while ((string1[index] || string2[index]) && index < max_chars)
+	{
+		if ((unsigned char)(string1[index]) == (unsigned char)(string2[index]))
+			index++;
+		else
+		{
+			if ((unsigned char)(string1[index]) < (unsigned char)(string2[index]))
+				return -1;
+			if ((unsigned char)(string1[index]) > (unsigned char)(string2[index]))
+				return 1;
+		}
+	}
+
+	return 0;
+}
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -9,3 +9,18 @@
 /*   Updated: 2023/09/23 01:24:11 by abolivar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+char *ft_strrchr(const char *str, int character_to_find)
+{
+	ssize_t str_length;
+
+	str_length = (ssize_t)ft_strlen(str) - 1;
+	while (str_length >= 0)
+	{
+		if (str[str_length] == (char)character_to_find)
+			return (&(((char *)str)[str_length]));
+		str_length--;
+	}
+	return (NULL);
+}
