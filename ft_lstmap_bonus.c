439d2b90cb620b827e14f7c5164637ed1fcef72a
VettRo <yggdrasilceoa.b@gmail.com>
2023-10-10 17:30:32 +0200

    bonus

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abolivar <abolivar@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 18:58:34 by abolivar          #+#    #+#             */
+/*   Updated: 2023/10/10 16:45:53 by abolivar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *first;
+	t_list *new;
+
+	if (!f || !del)
+		return (NULL);
+	first = NULL;
+	while (lst != NULL)
+	{
+		if (!(new = ft_lstnew((*f)(lst->content))))
+		{
+			while (first)
+			{
+				new = first->next;
+				(*del)(first->content);
+				free(first);
+				first = new;
+			}
+			lst = NULL;
+			return (NULL);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	return (first);
+}
