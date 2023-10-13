a1436f4a1add251346edfd36d64390cb12737422
Adam Bolivar Doulim <abolivar@c3r4s5.42malaga.com>
2023-10-13 20:14:43 +0200

    All deleted

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abolivar <abolivar@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/09 18:58:34 by abolivar          #+#    #+#             */
-/*   Updated: 2023/10/10 02:35:51 by abolivar         ###   ########.fr       */
+/*   Created: 2023/10/13 19:55:35 by abolivar          #+#    #+#             */
+/*   Updated: 2023/10/13 19:55:35 by abolivar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -26,16 +26,16 @@ int	ft_atoi(const char *str)
 	i = 0;
 	negative = 0;
 	num = 0;
-	while (ft_isspace(*(str + i)))
+	while (ft_isspace(str[i]))
 		i++;
-	if (*(str + i) == '-' || *(str + i) == '+')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (*(str + i) == '-')
+		if (str[i] == '-')
 			negative++;
 		i++;
 	}
-	while (*(str + i) >= '0' && *(str + i) <= '9')
-		num = (num * 10) + (*(str + i++) - 48);
+	while (str[i] >= '0' && str[i] <= '9')
+		num = (num * 10) + (str[i++] - 48);
 	if (negative)
 		num *= -1;
 	return (num);
