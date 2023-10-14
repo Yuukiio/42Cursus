d333173aa71dbdcfb2a72416446161f7de705e98
Adam Bolivar Doulim <abolivar@c2r1s3.42malaga.com>
2023-10-15 00:51:58 +0200

    calloc

--- LibftFunctions/ft_calloc.c
+++ LibftFunctions/ft_calloc.c
@@ -9,4 +9,13 @@
 /*   Updated: 2023/10/13 19:55:35 by abolivar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+void    *ft_calloc(size_t count, size_t size)
+{
+    void    *ptr;
+    if (ptr == NULL)
+        return (ptr);
+    ft_bzero(ptr, size * count);
+    return (ptr);
+}
